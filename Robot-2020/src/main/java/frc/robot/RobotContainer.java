7fe65fbc83e3b0c54ec3e94d5479c9a3e3571880
team2994controls <info@ocera.club>
2020-01-22 20:56:54 -0500

    manual shooter prototype with NEO

--- Robot-2020/src/main/java/frc/robot/RobotContainer.java
+++ Robot-2020/src/main/java/frc/robot/RobotContainer.java
@@ -20,16 +20,20 @@ import frc.commands.Shoot;
  * Add your docs here.
  */
 public class RobotContainer {
-    //Subsystems
-    private final Drivetrain m_drivetrain = new Drivetrain();
-    private final ShooterWheel m_shooterwheel = new ShooterWheel();
-    
     //Joystick and Gamepad
     public final Joystick joystick = new Joystick(0); 
     public final Joystick gamepad = new Joystick(1);
-
+   
+    //Subsystems
+    // private final Drivetrain m_drivetrain = new Drivetrain();
+    private final ShooterWheel m_shooterwheel = new ShooterWheel();
+    
     //Joystick and Gamepad buttons
-    private final JoystickButton jsButnShifter = new JoystickButton(this.joystick, 12);
+    // private final JoystickButton jsButnShifter = new JoystickButton(this.joystick, 12);
+    private final JoystickButton trigger = new JoystickButton(this.joystick, 1);
+    private final JoystickButton lvl1 = new JoystickButton(this.joystick, 12);
+    private final JoystickButton lvl2 = new JoystickButton(this.joystick, 10);
+    private final JoystickButton lvl3 = new JoystickButton(this.joystick, 8);
     
 
     //Contains subsystems, OI devices, and commands.
@@ -37,7 +41,7 @@ public class RobotContainer {
         
         configureButtons();
 
-        m_drivetrain.setDefaultCommand(new DefaultDrive(m_drivetrain, joystick));
+        // m_drivetrain.setDefaultCommand(new DefaultDrive(m_drivetrain, joystick));
         m_shooterwheel.setDefaultCommand(new Shoot(m_shooterwheel, gamepad));
     }
 
@@ -45,7 +49,11 @@ public class RobotContainer {
     private void configureButtons()
     {
         //Instant commands
-        this.jsButnShifter.whenPressed(new InstantCommand(m_drivetrain::highGear, m_drivetrain));
-		this.jsButnShifter.whenReleased(new InstantCommand(m_drivetrain::lowGear, m_drivetrain));
+        // this.jsButnShifter.whenPressed(new InstantCommand(m_drivetrain::highGear, m_drivetrain));
+        // this.jsButnShifter.whenReleased(new InstantCommand(m_drivetrain::lowGear, m_drivetrain));
+        this.trigger.whenPressed(new InstantCommand(m_shooterwheel::toggle, m_shooterwheel));
+        this.lvl1.whenPressed(new InstantCommand(m_shooterwheel::setLevelOne, m_shooterwheel));
+        this.lvl2.whenPressed(new InstantCommand(m_shooterwheel::setLevelTwo, m_shooterwheel));
+        this.lvl3.whenPressed(new InstantCommand(m_shooterwheel::setLevelThree, m_shooterwheel));
     }
 }
