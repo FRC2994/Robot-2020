ff533d4fe7065bb1aaf61cd617e691b9ea9be695
Aaron Jumarang <44382744+aaronjuma@users.noreply.github.com>
2022-02-12 15:55:57 -0500

    Update RobotContainer.java

--- Robot-2020/src/main/java/frc/robot/RobotContainer.java
+++ Robot-2020/src/main/java/frc/robot/RobotContainer.java
@@ -18,33 +18,33 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import frc.utils.GamepadRightAxis;
 
 //Subsystems Imports
-import frc.subsystems.ControlPanel;
+//import frc.subsystems.ControlPanel;
 import frc.subsystems.Drivetrain;
 import frc.subsystems.ShooterWheel;
-import frc.subsystems.Elevator;
-import frc.subsystems.ShooterHood;
-import frc.subsystems.VisionArduino;
+//import frc.subsystems.Elevator;
+//import frc.subsystems.ShooterHood;
+//import frc.subsystems.VisionArduino;
 import frc.subsystems.Intake;
-import frc.subsystems.Climber;
+//import frc.subsystems.Climber;
 
 //Commands Imports
 import frc.commands.DefaultDrive;
-import frc.commands.ShootSpeed;
+//import frc.commands.ShootSpeed;
 // import frc.commands.GoToColor;
 // import frc.commands.SpinControlPanel;
 // import frc.commands.FindTarget;
 import frc.commands.intakeArm;
-import frc.commands.AlignVision;
-import frc.commands.ControlPanelPiston;
+//import frc.commands.AlignVision;
+//import frc.commands.ControlPanelPiston;
 import frc.commands.Shoot;
 // import frc.commands.ShootAndFeed;
-import frc.commands.IntakeTrigger;
-import frc.commands.ShooterHoodChange;
-import frc.commands.Autonomous.SampleAuto;
-import frc.commands.Autonomous.AutoShooting;
+//import frc.commands.IntakeTrigger;
+//import frc.commands.ShooterHoodChange;
+//import frc.commands.Autonomous.SampleAuto;
+//import frc.commands.Autonomous.AutoShooting;
 // import frc.commands.BallElevator;
-import frc.commands.IntakeAndElevator;
-import frc.commands.Autonomous.SixBallAuto;
+//import frc.commands.IntakeAndElevator;
+//import frc.commands.Autonomous.SixBallAuto;
 
 /**
  * Add your docs here.
@@ -58,15 +58,15 @@ public class RobotContainer {
    
     //Subsystems
     private final Drivetrain drivetrain = new Drivetrain();
-    private final ControlPanel controlpanel = new ControlPanel(); 
+    //private final ControlPanel controlpanel = new ControlPanel(); 
     private final ShooterWheel shooterwheel = new ShooterWheel();
-    private final Elevator elevator = new Elevator();
-    private final ShooterHood shooterhood = new ShooterHood();
-    private final VisionArduino vision = new VisionArduino();
+    //private final Elevator elevator = new Elevator();
+    //private final ShooterHood shooterhood = new ShooterHood();
+    //private final VisionArduino vision = new VisionArduino();
     private final Intake intake = new Intake();
     //TODO: figure out if the hopper even needs to be a subsystem now
     // private final Hopper hopper = new Hopper();
-    private final Climber climber = new Climber();
+    //private final Climber climber = new Climber();
 
     private UsbCamera camera;
     private GamepadRightAxis rightAxis = new GamepadRightAxis(gamepad);
@@ -110,11 +110,11 @@ public class RobotContainer {
         /*SHOOTER*/
         this.gpButnShoot.whileHeld(new InstantCommand(shooterwheel::shoot, shooterwheel)); //Manual
         this.gpButnShoot.whenReleased(new InstantCommand(shooterwheel::stopMotor, shooterwheel));
-        this.gpButnShooter.whileHeld(new Shoot(elevator, shooterwheel)); //With the elevator
+        //this.gpButnShooter.whileHeld(new Shoot(elevator, shooterwheel)); //With the elevator
         // this.gpButnShootFeedButton.whileHeld(new ShootAndFeed(shooterwheel, elevator));
         /*ELEVATOR*/
-        this.gpButnManualElevator.whileHeld(new InstantCommand(elevator::startMotor, elevator)); //Manual
-        this.gpButnManualElevator.whenReleased(new InstantCommand(elevator::stopMotor, elevator));
+        //this.gpButnManualElevator.whileHeld(new InstantCommand(elevator::startMotor, elevator)); //Manual
+        //this.gpButnManualElevator.whenReleased(new InstantCommand(elevator::stopMotor, elevator));
         // this.gpButnManualElevator.whileHeld(new BallElevator(elevator));
 
         /*CONTROL PANEL*/
@@ -122,31 +122,31 @@ public class RobotContainer {
         // this.jsButnDetectColour.whileHeld(new GoToColor(controlpanel));
         // this.jsButnRotationControl.whileHeld(new SpinControlPanel(controlpanel));
         // this.jsButnRaiseAndLowerControlPanel.whenPressed(new ControlPanelPiston(controlpanel));
-        jsButnRotationControl.whileHeld(new InstantCommand(controlpanel::moveMotor, controlpanel));
-        jsButnRotationControl.whenReleased(new InstantCommand(controlpanel::stopMotor, controlpanel));
+        //jsButnRotationControl.whileHeld(new InstantCommand(controlpanel::moveMotor, controlpanel));
+        //jsButnRotationControl.whenReleased(new InstantCommand(controlpanel::stopMotor, controlpanel));
         /*CLIMBER*/
-        this.jsButnRaiseClimb.whileHeld(new InstantCommand(climber::openLoopUp, climber));
-        this.jsButnRaiseClimb.whenReleased(new InstantCommand(climber::stopMotor, climber));
-        this.jsButnLowerClimb.whileHeld(new InstantCommand(climber::openLoopDown, climber));
-        this.jsButnLowerClimb.whenReleased(new InstantCommand(climber::stopMotor, climber));
+        //this.jsButnRaiseClimb.whileHeld(new InstantCommand(climber::openLoopUp, climber));
+        //this.jsButnRaiseClimb.whenReleased(new InstantCommand(climber::stopMotor, climber));
+        //this.jsButnLowerClimb.whileHeld(new InstantCommand(climber::openLoopDown, climber));
+        //this.jsButnLowerClimb.whenReleased(new InstantCommand(climber::stopMotor, climber));
         /*HOPPER*/
         // this.gpButnHopperDisturber.whileHeld(new InstantCommand(hopper:: HopperDisturberExtend, hopper));
         // this.gpButnHopperDisturber.whenReleased(new InstantCommand(hopper:: HopperDisturberIntake, hopper));
         /*INTAKE*/
         // Removed this, this is a trigger now
-        this.gpButnIntake.whileActiveOnce(new IntakeAndElevator(elevator, gamepad, intake));
+        //this.gpButnIntake.whileActiveOnce(new IntakeAndElevator(elevator, gamepad, intake));
         this.gpButnIntakeDownAndUp.whenPressed(new intakeArm(intake));
         /*PIXYCAM*/
-        this.gpButnRunPixyCam.whileHeld(new AlignVision(drivetrain, vision));
+        //this.gpButnRunPixyCam.whileHeld(new AlignVision(drivetrain, vision));
     }
 
 
     /*AUTONOMOUS STUFF*/
     // Command sampleAuto = new SampleAuto(drivetrain, elevator, intake, vision, shooterwheel);
     
-    public Command getAutoCommand() {
+   // public Command getAutoCommand() {
         // Command selectedAuto = new AutoShooting(drivetrain, elevator, shooterwheel);
-        Command selectedAuto = new SixBallAuto(drivetrain, elevator, intake, shooterwheel, vision);
+        //Command selectedAuto = new SixBallAuto(drivetrain, elevator, intake, shooterwheel, vision);
         // switch(whichAuto){
         //     /*Lines up to target, shoots, goes back to get more balls*/
         //     case 0:     selectedAuto = new AutoShooting(drivetrain, elevator, vision, shooterwheel);
@@ -160,17 +160,17 @@ public class RobotContainer {
         //     default:    selectedAuto = new AutoShooting(drivetrain, elevator, vision, shooterwheel);
         //                 break;
         // }
-        return selectedAuto;
-    }
+        //return selectedAuto;
+    //}
 
     public void enableDefaultCommands() {
         this.drivetrain.setDefaultCommand(new DefaultDrive(drivetrain, joystick));
-        this.shooterwheel.setDefaultCommand(new ShootSpeed(this.shooterwheel, this.gamepad));
+        //this.shooterwheel.setDefaultCommand(new ShootSpeed(this.shooterwheel, this.gamepad));
         // this.controlpanel.setDefaultCommand(new GoToColor(this.controlpanel));
         // climber.enable();
         // this.intake.setDefaultCommand(new IntakeTrigger(intake, gamepad));
         // this.intake.setDefaultCommand(new IntakeAndElevator(elevator, gamepad, intake));
-        this.shooterhood.setDefaultCommand(new ShooterHoodChange(shooterhood, gamepad));
+        //this.shooterhood.setDefaultCommand(new ShooterHoodChange(shooterhood, gamepad));
     }
 
     public double getPDPCurrent(int channel) {
